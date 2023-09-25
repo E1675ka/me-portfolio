e9f1efc7280948aade281b39e26927038de47df8
E1675ka <jacobangel254@gmail.com>
2023-09-26 01:54:27 +0300

    rename screenshots names 2

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,17 +8,13 @@ export function Projects() {
         <a href="https://ask-web-hosting-site.netlify.app/">
           <img
             className="ask-image"
-            src="public/images/2023-09-25 21_59_39-A.S.K hosting website.png"
+            src="/images/A.S.K hosting website.png"
             alt="web hosting clone pic"
           />
           <button>web hosting clone</button>
         </a>
         <a href="https://frontend-gpt3-web-landing-page-clone.netlify.app/">
-          <img
-            className="gpt"
-            src="public/images/2023-09-25 23_52_51-React App.png"
-            alt="gpt3 picture"
-          />
+          <img className="gpt" src="/images/React App.png" alt="gpt3 picture" />
           <button>gpt3 clone landing page</button>
         </a>
       </div>
